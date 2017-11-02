590ecd4dd5ad5c9dd562a6853da4e68053c0179e
xizhen dai <dai.xizhen@vanderbilt.edu>
2017-11-02 11:47:21 -0500

    implement is_member_of?(group)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171101220058) do
+ActiveRecord::Schema.define(version: 20171102155338) do
+
+  create_table "group_relationships", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "group_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "title"
